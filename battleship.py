f67ee508bbe0d1f6849d9a8a84209d0be717fecd
nickmurph <nfmurphy@buffalo.edu>
2022-10-29 04:08:34 -0600

    further progress on enhanced grid targeting method

--- battleship.py
+++ battleship.py
@@ -66,7 +66,9 @@ missedShot = Fore.LIGHTCYAN_EX + '00' + Style.RESET_ALL
 userTargets = []
 enemyTargets = []
 hitCounts = [0,0]
-enemyGridStartPos = [0,-1]
+enemyGridPos = [0,-1]
+rdToggle = False
+
 #enemyGridStartPos = generate_random_target()
 
 # hash map for retrieving size of ships with their name as a key
@@ -494,25 +496,31 @@ def player_auto_turn_input():
 
 #currentPos is a tuple of 
 def traverseGrid(currentPos, leftOrRight, upOrDown, jumpInterval):
+    global rdToggle
     nextTarget = currentPos
     colDirection = leftOrRight
     rowDirection = upOrDown
     jump = jumpInterval
 
     if colDirection == "right" and rowDirection == "down":
+        if rdToggle:
+            colStartVal = 1
+        else:
+            colStartVal = 0
+        
         nextTarget[1] = nextTarget[1] + jump
         if nextTarget[1] > 9:
-            nextTarget[0] = nextTarget[0] + jump
-            nextTarget[1] = 0
-
-    elif colDirection == "left" and rowDirection == "down":
-        nextTarget[1] = nextTarget[1] - jump
-        if nextTarget[1] > 9:
-            nextTarget[0] = nextTarget[0] + jump
-            nextTarget[1] = 0
+            nextTarget[0] = nextTarget[0] + 1
+            rdToggle = not rdToggle
+            nextTarget[1] = colStartVal
 
     
-    return [0,0]
+    return nextTarget
+    #print(nextTarget)
+    # if nextTarget == [9,(8 or 9 or 10)]:
+    #     flag = False
+    
+    #return [0,0]
 
 
 # Each time the enemy has a turn, they will select a coordinate in the while loop and then that shot is validated as either a miss or hot
@@ -529,16 +537,22 @@ def enemy_turn_input():
     while enemyFindingTarget:
 
         #random search
-        currentTarget = generate_random_target()
-        targetRow = currentTarget[0]
-        targetCol = currentTarget[1]
-        if currentTarget not in enemyTargets:
-            enemyTargets.append(currentTarget)
-            enemyFindingTarget = False   
+        # currentTarget = generate_random_target()
+        # targetRow = currentTarget[0]
+        # targetCol = currentTarget[1]
+        # if currentTarget not in enemyTargets:
+        #     enemyTargets.append(currentTarget)
+        #     enemyFindingTarget = False   
 
-        #grid search
-        # enemyGridStartPos = [0,-1]
-        
+        # grid search
+        # 
+        global enemyGridPos 
+        enemyGridPos = traverseGrid(enemyGridPos,"right","down",2)
+        targetRow = enemyGridPos[0]
+        targetCol = enemyGridPos[1]
+        if enemyGridPos not in enemyTargets:
+            enemyGridPos.append(enemyGridPos)
+            enemyFindingTarget = False   
 
         #
         # do stuff here
@@ -552,17 +566,29 @@ def enemy_turn_input():
     # proceeding with the coordinate previously generated as our firing solution
     #  if no ship is there, mark the corresponding square in the gameBoard as a missed shot
     # if the coordinate is not empty sea, then it must be a hit
-    # increment the hitcount and inform the player the AI has struck a ship 
+    # increment the hitcount and inform the player the AI has struck a ship
+    
+    #print(targetRow,targetCol)
+    if targetRow == 10: # TEMP CODE
+        global rdToggle
+        rdToggle = not rdToggle
+        enemyGridPos = [0,0]
+        targetRow = enemyGridPos[0]
+        targetCol = enemyGridPos[1]
+    #print(targetRow,targetCol)
+
     if gameBoard[targetRow][targetCol] == openSea:
         gameBoard[targetRow][targetCol] = missedShot
-        alphaRandMiss = coord_to_alphanumeric(currentTarget)
+        #alphaRandMiss = coord_to_alphanumeric(currentTarget)
+        alphaRandMiss = coord_to_alphanumeric(enemyGridPos)
         global enemyLastTurn
         enemyLastTurn = f"THE ENEMY MISSED! Their shot landed at {alphaRandMiss}!"
         print("")
         print("")
     else:
         gameBoard[targetRow][targetCol] = shipStruck
-        alphaRandHit = coord_to_alphanumeric(currentTarget)
+        #alphaRandHit = coord_to_alphanumeric(currentTarget)
+        alphaRandHit = coord_to_alphanumeric(enemyGridPos)
         hitCounts[1] = hitCounts[1] + 1
         enemyLastTurn = f"THE ENEMY SCORED A HIT! Your ship was struck at {alphaRandHit}!"
         print("")
