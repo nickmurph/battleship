e462d619ddc361e2e76f7052843e3e6afb9ff515
nickmurph <nfmurphy@buffalo.edu>
2022-11-03 03:01:07 -0600

    fully implemented 4-diagonal grid hopping for enemy AI
    -AI can now grid hop in a checkboard pattern in all 4 diagonals ( L/R, U/D)
    -parameterized enemy_turn_input
    -added new vars to the BattleshipInstance class for AI targeting
    -logic in BattleshipInstance to randomize which diagonal direction to grid hop in, changing each game
    -expanded traverseGrid() to account for these new diagonal options (needs to refactor/minimize)

--- bs_globalvars.py
+++ bs_globalvars.py
@@ -2,9 +2,9 @@
 #
 # CLASS HOLDING ALL OF THE VARIABLES RELATED TO THE GAME BOARD AND ITS STATUS
 #
-# ██
+# 
 
-
+import random
 import colorama
 from colorama import Fore, Style, init
 colorama.init(convert=True)
@@ -14,27 +14,41 @@ colorama.init(convert=True)
 class BattleshipInstance:
     def __init__(self):
         # constants
-        self.OPEN_SEA = Fore.LIGHTCYAN_EX + '__' + Style.RESET_ALL
-        #self.SHIP_BODY = Fore.GREEN + 'QQ' + Style.RESET_ALL
+        self.OPEN_SEA = Fore.LIGHTCYAN_EX + '__' + Style.RESET_ALL  #formerly used ██, but __ looks cleaner
+        self.SHIP_BODY = Fore.GREEN + 'QQ' + Style.RESET_ALL    #currently deprecated but may use in future expansion
         self.SHIP_STRUCK = Fore.RED + 'XX' + Style.RESET_ALL
         self.MISSED_SHOT = Fore.LIGHTCYAN_EX + '00' + Style.RESET_ALL
-        # variables
+        
+        # board variables
         self.playerBoard = []
         self.enemyBoard = []
         self.targetBoard = []
         self.userTargets = []
         self.enemyTargets = []
         self.hitCounts = [0,0]
-        self.enemyGridPos = [0,-1]
-        self.enemyMode = "grid" # random, grid, or kill are the valid choices
-        self.rdToggle = False
         self.turnCounter = 0
         self.userLastTurnMessage = None
         self.enemyLastTurnMessage = None
+
+        # AI targeting variables
+        self.levelOfAI = 2 #1, 2, or 3. 1 is random, 2 is grid hopper, 3 is grid hopper with hunter-killer search
+        self.enemyGridPos = [0,-1]
+        self.enemyMode = "grid" # random, grid, or kill are the valid choices
+        self.rdToggle = False
+        self.enemyTraverseLeftOrRight = "right" # "left" or "right" are valid choices
+        self.enemyTraverseUpOrDown = "down" # "up" or "down" are valid choices
+
+        # ship variables
         self.shipsHashMap = {}
         self.shipNameList = []
+
+        # func calls 
         self.build_ship_lists() 
         self.build_initial_boards()
+        self.randomizeEnemyTraversalDirections()
+        self.overwriteDefaultsForEnemyTargeting() 
+
+
 
     def build_ship_lists(self):
         self.shipsHashMap['Carrier'] = 5, Fore.GREEN + 'AC' + Style.RESET_ALL
@@ -55,6 +69,8 @@ class BattleshipInstance:
         self.playerLastTurnMessage = None
         self.enemyLastTurnMessage = None
         self.reset_boards()
+        self.randomizeEnemyTraversalDirections()
+        self.overwriteDefaultsForEnemyTargeting() 
 
     def build_initial_boards(self):
         for x in range(10):
@@ -72,3 +88,34 @@ class BattleshipInstance:
     def eitherPlayerHas17Hits(self):
         return (self.hitCounts[0] == 17 or self.hitCounts[1] == 17)
 
+    def overwriteDefaultsForEnemyTargeting(self):
+        if self.enemyTraverseLeftOrRight == "right":
+            if self.enemyTraverseUpOrDown == "down":
+                self.enemyGridPos = [0,-1] # RD
+            else:
+                self.enemyGridPos = [9,-1] # RU
+        elif self.enemyTraverseLeftOrRight == "left":
+            if self.enemyTraverseUpOrDown == "down":
+                self.enemyGridPos = [0,10] # LD
+            else:
+                self.enemyGridPos = [9,10] # LU
+
+    def randomizeEnemyTraversalDirections(self):
+        # 4 total options: RD, RU, LD, LU
+        # let R = 1, L = 0. D = 1, U = 0
+        # can view each combination as a 2-bit binary number
+
+        randBin = ""
+        for i in range(2):
+            curRand = random.randint(0,1)
+            randBin = randBin + str(curRand)
+
+        if randBin[0] == '1':
+            self.enemyTraverseLeftOrRight = "right"
+        elif randBin[0] == '0':
+            self.enemyTraverseLeftOrRight = "left"   
+        
+        if randBin[1] == '1':
+                self.enemyTraverseUpOrDown = "down"
+        elif randBin[1] == '0':
+                self.enemyTraverseUpOrDown = "up"
