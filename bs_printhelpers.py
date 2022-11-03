82c24092b249cb9f163dd8ddfd2e0fbb495f92a6
nickmurph <nfmurphy@buffalo.edu>
2022-11-03 01:05:03 -0600

    comments for main game loop logic, some var name tweaking

--- bs_printhelpers.py
+++ bs_printhelpers.py
@@ -10,9 +10,9 @@ import bs_playeractions
 import bs_global_hub
 
 
-def printGameBoard():
+def printPlayerBoard():
     game = bs_global_hub.get_game_pointer()
-    gameBoard = game.gameBoard
+    playerBoard = game.playerBoard
     print("           YOUR AREA OF OPERATIONS         ")
     print("")
     print("    1   2   3   4   5   6   7   8   9   1O ")
@@ -21,50 +21,50 @@ def printGameBoard():
         curLetter = chr(i+65)
         print(curLetter + "   ", end="")
         if i == 0:
-            print(*gameBoard[i], sep='  ')
+            print(*playerBoard[i], sep='  ')
         elif i == 1:
-            print(*gameBoard[i], sep='  ', end='')
+            print(*playerBoard[i], sep='  ', end='')
             print("                           KEY:")
         elif i == 2:
-            print(*gameBoard[i], sep='  ', end='')
+            print(*playerBoard[i], sep='  ', end='')
             print("                           AC = Aircraft Carrier")
         elif i == 3:
-            print(*gameBoard[i], sep='  ', end='')
+            print(*playerBoard[i], sep='  ', end='')
             print("                           BS = Battleship")
         elif i == 4:
-            print(*gameBoard[i], sep='  ', end='')
+            print(*playerBoard[i], sep='  ', end='')
             print("                           CR = Crusier")
         elif i == 5:
-            print(*gameBoard[i], sep='  ', end='')
+            print(*playerBoard[i], sep='  ', end='')
             print("                           SB = Submarine")
         elif i == 6:
-            print(*gameBoard[i], sep='  ', end='')
+            print(*playerBoard[i], sep='  ', end='')
             print("                           DT = Destroyer")
         elif i == 7:
-            print(*gameBoard[i], sep='  ', end='')
+            print(*playerBoard[i], sep='  ', end='')
             print("                           XX = Damaged Ship")
         elif i == 8:
-            print(*gameBoard[i], sep='  ', end='')
+            print(*playerBoard[i], sep='  ', end='')
             print("                           00 = Missed Shot")
         elif i == 9:
-            print(*gameBoard[i], sep='  ', end='')
+            print(*playerBoard[i], sep='  ', end='')
             print("                           ██ = Empty Ocean")
         else:
-            print(*gameBoard[i], sep='  ')
+            print(*playerBoard[i], sep='  ')
 
 
 
-def printGameBoardNoKey():
+def printPlayerBoardNoKey():
     # game = battleship.get_current_game()
     game = bs_global_hub.get_game_pointer()
     # game = main.get_bs_logic()
-    gameBoard = game.gameBoard
+    playerBoard = game.playerBoard
 
     print("    1   2   3   4   5   6   7   8   9   1O ")
     for i in range(10):
         curLetter = chr(i+65)
         print(curLetter + "   ", end="")
-        print(*gameBoard[i], sep='  ')
+        print(*playerBoard[i], sep='  ')
 
 
 
@@ -113,7 +113,7 @@ def printTargetBoard():
 def input_spacer_with_board():
     print("")
     print("")
-    printGameBoardNoKey()
+    printPlayerBoardNoKey()
     print("")
     print("")
 
@@ -132,14 +132,14 @@ def clear_and_print_both_boards():
     input_spacer_no_board()
     print_ascii_logo()
     input_spacer_no_board()
-    printGameBoard()
+    printPlayerBoard()
     input_spacer_no_board()
     printTargetBoard()
     input_spacer_no_board()
 
 def print_both_boards():
     input_spacer_no_board()
-    printGameBoard()
+    printPlayerBoard()
     input_spacer_no_board()
     printTargetBoard()
     input_spacer_no_board()
